5b88a973f2dcc150a421b6ffc9edced89f096675
Tata Wedha Hutama <tatawedha@gmail.com>
2022-04-30 09:22:24 +0700

    fix:display login figma

--- src/views/Home New.vue
+++ src/views/Home New.vue
@@ -1,47 +1,46 @@
 <template>
   <div
     class="container-fluid"
+    :style="{
+      'background-image': 'url(https://picsum.photos/seed/picsum/1000/1000)',
+    }"
   >
-    <!-- <h1 class="title text-center py-4">SSO POLTEKES</h1> -->
+    <h1 class="title text-center py-4">SSO POLTEKES</h1>
 
     <div class="row">
       <div class="col d-flex justify-content-center">
         <div class="card cb1 m-2 text-center">
           <div class="card-body">
             <!-- <span class="card-number">01</span> -->
-            <img src="@/assets/logo-poltekes.jpg" alt="">
 
-            <h5 class="SSO mb-4">Single Sign On (SSO)</h5>
-            <h5 class="card-title mb-4">Poltekes Semarang</h5>
+            <h5 class="card-title mb-4">Login</h5>
             <div class="mb-3">
-              <!-- <label for="exampleInputEmail1" class="form-label"
+              <label for="exampleInputEmail1" class="form-label"
                 >Alamat Email</label
-              > -->
+              >
               <input
                 type="email"
                 class="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
                 v-model="data.username"
-                placeholder="Username/e-mail"
               />
             </div>
             <div class="mb-3">
-              <!-- <label for="exampleInputpassword" class="form-label"
+              <label for="exampleInputpassword" class="form-label"
                 >Password</label
-              > -->
+              >
               <input
                 type="password"
                 class="form-control"
                 id="exampleInputpassword"
                 v-model="data.password"
-                placeholder="password"
                 @keydown.enter.prevent="login()"
               />
             </div>
             <div class="mb-3 mt-3">
               <div class="d-flex justify-content-center">
-                <div class="col-12">
+                <div class="col-6">
                   <button
                     href="#"
                     class="btn btn-outline-primary"
@@ -53,22 +52,21 @@
               </div>
             </div>
 
-            <div class="register mt-3">
-              <div class="row register=box">
-                <div class="col-6">Belum Memiliki Akun?</div>
+            <div class="mt-3">
+              <div class="row">
                 <div
-                  class="register-text col-6"
+                  class="register col-5"
                   @click="$router.push({ path: '/register' })"
                 >
-                  Daftar Sekarang
+                  Register
                 </div>
-                <!-- <div class="col-2">|</div>
+                <div class="col-2">|</div>
                 <div
                   class="lupa col-5"
                   @click="$router.push({ path: '/lupaPassword' })"
                 >
                   Lupa Password
-                </div> -->
+                </div>
               </div>
             </div>
           </div>
@@ -152,12 +150,6 @@ export default {
   /* color: aqua; */
 }
 
-.SSO{
-  font-size: 30px;
-  line-height: 38px;
-  font-weight: 600;
-  color: #101828;
-}
 .title {
   margin: 0 auto;
   width: 100%;
@@ -168,9 +160,8 @@ export default {
 
 .card {
   width: 90%;
-  max-width: 440px;
+  max-width: 400px;
   padding: 5rem 2.5rem;
-  height: 560px;
 
   border-radius: 1rem;
   border: 1px solid transparent;
@@ -179,17 +170,16 @@ export default {
   backdrop-filter: blur(1rem);
   box-shadow: 1.3rem 1.3rem 1.3rem rgba(0, 0, 0, 0.5);
 
-  /* border-top-color: rgba(225, 225, 225, 0.5);
+  border-top-color: rgba(225, 225, 225, 0.5);
   border-left-color: rgba(225, 225, 225, 0.5);
   border-bottom-color: rgba(225, 225, 225, 0.1);
-  border-right-color: rgba(225, 225, 225, 0.1); */
+  border-right-color: rgba(225, 225, 225, 0.1);
 }
 
 .card:hover {
   width: 90%;
-  max-width: 440px;
+  max-width: 400px;
   padding: 5rem 2.5rem;
-  height: 560px;
 
   border-radius: 1rem;
   border: 1px solid transparent;
@@ -203,33 +193,20 @@ export default {
   backdrop-filter: blur(1rem);
   box-shadow: 1.3rem 1.3rem 1.3rem rgba(0, 0, 0, 0.5);
 
-  /* border-top-color: rgba(225, 225, 225, 0.5);
+  border-top-color: rgba(225, 225, 225, 0.5);
   border-left-color: rgba(225, 225, 225, 0.5);
   border-bottom-color: rgba(225, 225, 225, 0.1);
-  border-right-color: rgba(225, 225, 225, 0.1); */
+  border-right-color: rgba(225, 225, 225, 0.1);
 }
 .btn {
   width: 100%;
-  background-color: #027A48;
-  color: #FFFFFF;
 }
-.register{
-  font-size: 14px;
-}
-
-.register-box{
-  justify-content: space-between;
-}
-.register-text{
-  color: #027A48;
-  
-}
-/* .register:hover {
+.register:hover {
   transform: scale(1.1);
   font-size: 16px;
   font-weight: 500;
   color: blue;
-} */
+}
 
 .lupa:hover {
   transform: scale(1.1);
